fb072b84c886af2dc263e4d1d207664c5d3d91a1
Quentin HANSMANN <122374896+QuentinHansmann@users.noreply.github.com>
2023-05-04 19:19:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
-init
+import tkinter as tk
+from tkinter import filedialog
+import shutil
+
+root = tk.Tk()
+
+def select_file():
+    # Open a dialog box to select a file
+    file_path = filedialog.askopenfilename()
+    destination = '/path/to/destination/folder'
+    # Copy the file to the destination folder
+    shutil.copy(file_path, destination)
+
+# Create a button to select a file
+select_button = tk.Button(root, text="Select File", command=select_file)
+select_button.pack()
+
+root.mainloop()
